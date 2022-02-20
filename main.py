ee6d963575f4db36100c92a5f8ddd13d7aebb35c
Захаров Павел <pzakharov83@gmail.com>
2022-02-20 21:02:46 +0300

    changed structure to fetch

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 import aiohttp
 import asyncio
+import sys
+
+from adapters.inosmi_ru import sanitize
+
+
+if (sys.version_info[0] == 3 and sys.version_info[1] >= 8 and
+        sys.platform.startswith('win')):
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
 
 async def fetch(session, url):
@@ -10,8 +18,10 @@ async def fetch(session, url):
 
 async def main():
     async with aiohttp.ClientSession() as session:
-        html = await fetch(session, 'http://example.com')
-        print(html)
+        html = await fetch(
+            session, 'https://inosmi.ru/20220203/mks-252849859.html'
+        )
+        print(sanitize(html, plaintext=True))
 
 
 asyncio.run(main())
